48dd1c2041cfb422bdc2b0d924e47514e43bbf13
Rahul Kumar Sharma <rahul.sharma@example.com>
2025-01-29 16:38:17 +0530

    initialization of project

---
+++ src/main/java/com/sharma/finance/sharmaFinance/SharmaFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.sharma.finance.sharmaFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharmaFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharmaFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharma/finance/sharmaFinance/SharmaFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharma.finance.sharmaFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharmaFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
